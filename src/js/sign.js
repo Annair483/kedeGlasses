f51407d0cffcb5cc245aaea6d186f8eb2cb3858c
Ann <446331381@qq.com>
2019-02-12 10:24:34 +0800

    扫码登录完成

--- src/js/sign.js
+++ src/js/sign.js
@@ -1,3 +1,25 @@
- jQuery(function($){
-     
+ jQuery(function ($) {
+     let $title = $('.l_r_title label');
+     let $loginWindow = $('.j_saoma,.j_zhanghao');
+     let $Jdown_app = $('#Jdown_app');
+     //登录窗口选项切换
+     $title.eq(0).addClass('active');
+     $loginWindow.eq(0).show();
+     $.each($title, function (idx, item) {
+         $(item).on('click', function () {
+             $.each($title, function (idx, item) {
+                 $(item).removeClass('active');
+                 $loginWindow.eq(idx).hide();
+             })
+             $(item).addClass('active');
+             $loginWindow.eq(idx).show();
+         })
+     })
+     //下载app图标切换
+     $Jdown_app.eq(0).mouseover(function () {
+         $(this).hide().next().show();
+     }).next().mouseout(function () {
+         $(this).hide().prev().show();
+     })
+     //账号登录
  })
