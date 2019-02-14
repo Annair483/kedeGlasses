233a10e3c3e02e11b60465e200f018cd709acadb
Ann <446331381@qq.com>
2019-02-14 09:49:01 +0800

    首页tab切换完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 jQuery(function ($) {
     let $wide_banner = $(".wide_banner");
     let $navigation = $('.slideshortcut a');
-    let $hp_menu =  $(".hp_menu");
+    let $hp_menu = $(".hp_menu");
     let $ul1 = $hp_menu.eq(0).find('ul');
     let $ul2 = $hp_menu.eq(1).find('ul');
     //hover大轮播图 箭头出现
@@ -16,19 +16,22 @@ jQuery(function ($) {
     }, function () {
         $navigation.css('opacity', 0.6);
     })
-    bigBanner()
     //大轮播图轮播
+    var now = 0; //当前可视区的li下标
+    var $wide_banner_bg = $(".wide_banner_bg");
+    let colorArr = ['#C83841', '#FFFDF1', '#FAF9FF', '#6E5366', '#F05A4B', '#D6E8FE'];
+    bigBanner()
+
     function bigBanner() {
-        let $wide_banner_bg = $(".wide_banner_bg");
         let $imglist = $('.slidebox li');
-        let colorArr = ['#C83841', '#FFFDF1', '#FAF9FF', '#6E5366', '#F05A4B', '#D6E8FE'];
+        
         let $page = $('.light span');
         $wide_banner_bg.css('background-color', colorArr[0]);
         $imglist.css('left', 0);
         $imglist.hide();
         $imglist.eq(0).show();
         //2.开定时器：每次轮播一个图
-        var now = 0; //当前可视区的li下标
+        // var now = 0; //当前可视区的li下标
         var timer = null;
         timer = setInterval(next, 5000); //每隔两秒切下一张
         function next() {
@@ -77,6 +80,7 @@ jQuery(function ($) {
                 //旧图：放到左边
                 $imglist.eq(now).fadeOut(300);
                 //新图：快速放在右侧，再挪进来
+                $wide_banner_bg.css('background-color', colorArr[index]);
                 $imglist.eq(index).fadeIn(300);
                 now = index;
             }
@@ -85,6 +89,7 @@ jQuery(function ($) {
                 //旧图：放到右边
                 $imglist.eq(now).fadeOut(300);
                 //新图：快速放在左侧，再挪进来
+                $wide_banner_bg.css('background-color', colorArr[index]);
                 $imglist.eq(index).fadeIn(300);
                 now = index;
             }
@@ -96,7 +101,7 @@ jQuery(function ($) {
     smallbanner('.wide_f_ban');
     smallbanner('.infcp_f_cont');
     //小banner图轮播
-    function smallbanner(ele, pageEle) {
+    function smallbanner(ele) {
         let $box = $(ele);
         let $li = $box.find('.infcptop>li');
         let $pageLi = $(ele).find('.infcp_baniu li');
@@ -224,42 +229,42 @@ jQuery(function ($) {
         tabMz()
     }
     //秒杀和值得买选项卡
-    function tabMz(){
+    function tabMz() {
         let $zw = $('.zw a');
-        let $dsfw  = $(".dsfw a");
-        $zw.mouseover(function(){
-            $(this).css('background-position','0px 0px')
-             $dsfw.css('background-position','0px -91px')
-             $hp_menu.eq(0).show().siblings().hide();
+        let $dsfw = $(".dsfw a");
+        $zw.mouseover(function () {
+            $(this).css('background-position', '0px 0px')
+            $dsfw.css('background-position', '0px -91px')
+            $hp_menu.eq(0).show().siblings().hide();
         })
-        $dsfw.mouseover(function(){
-            $(this).css('background-position','0px -273px')
-            $zw.css('background-position','0px -182px')
+        $dsfw.mouseover(function () {
+            $(this).css('background-position', '0px -273px')
+            $zw.css('background-position', '0px -182px')
             $hp_menu.eq(1).show().siblings().hide();
         })
     }
     //1F渲染
-    function oneFGods(res){
+    function oneFGods(res) {
         let $img = $('.wide_4f_li_img img');
         let $wide_4f_li_name = $('.wide_4f_li_name');
         let $wide_4f_li_price = $('.wide_4f_li_price');
-        $.each(res,function(idx,item){
-            $img.eq(idx).attr('src',item.url);
+        $.each(res, function (idx, item) {
+            $img.eq(idx).attr('src', item.url);
             $wide_4f_li_name.eq(idx).html(item.godsName);
             $wide_4f_li_price.eq(idx).html(`￥${item.price}`)
         })
     }
     //2F渲染
-    function twoFGods(res){
+    function twoFGods(res) {
         let $hotImg = $('.wide_f_hot_img img');
         let $wide_f_hot_name = $('.wide_f_hot_name');
         let $wide_f_hot_price = $('.wide_f_hot_price');
         let $img = $('.wide_f_item_img');
         let $wide_f_item_name = $('.wide_f_item_name');
         let $wide_f_item_price = $('.wide_f_item_price');
-        $.each(res,function(idx,item){
-            $img.eq(idx).attr('src',item.url).attr('alt',item.godsName);
-            $hotImg.eq(idx).attr('src',item.url).attr('alt',item.godsName);
+        $.each(res, function (idx, item) {
+            $img.eq(idx).attr('src', item.url).attr('alt', item.godsName);
+            $hotImg.eq(idx).attr('src', item.url).attr('alt', item.godsName);
             $wide_f_item_name.eq(idx).html(item.godsName);
             $wide_f_hot_name.eq(idx).html(item.godsName);
             $wide_f_item_price.eq(idx).html(`￥${item.price}`)
@@ -268,30 +273,55 @@ jQuery(function ($) {
     }
     //门店楼层tab切换
     let $right = $('.right>li');
-    let $tit =$('.tit>li');
+    let $tit = $('.tit>li');
     let $left = $('.left>li');
+    var i=1;
     shop()
-    function shop(){
+    //门店一级选项卡
+    function shop() {
         let $tab = $(".tab li");
-        $tab.mouseover(function(){
-            let i = $(this).index();
+        $tab.mouseover(function () {
+            i = $(this).index();
             $(this).addClass('on').siblings().removeClass('on');
-            $right.eq(i*2).show().addClass('ass').siblings().hide().removeClass('ass');
+            $right.eq(i * 2).show().addClass('ass').siblings().hide().removeClass('ass');
             $tit.eq(i).show().siblings().hide();
             leftTab(i)
         })
-    }
-    let i = 1
-    leftTab()
-    function leftTab(i){
 
-        $left.eq(0).mouseover(function(){
-            $(this).attr('class','info_on').next().attr('class','map')
+    }
+    leftTab()
+    //门店二级选项卡
+    function leftTab() {
+        $left.eq(0).mouseover(function () {
+            $(this).attr('class', 'info_on').next().attr('class', 'map');
             $right.filter('.ass').show().siblings().hide();
         })
-        $left.eq(1).mouseover(function(){
-            $(this).attr('class','map_on').prev().attr('class','info')
+        $left.eq(1).mouseover(function () {
+            $(this).attr('class', 'map_on').prev().attr('class', 'info');
             $right.filter('.ass').next().show().siblings().hide();
         })
     }
+    //文章选项卡
+    let $inlaf_tis = $(".inlaf-tis li");
+    let $inlaf_l_m = $(".inlaf-l-nm");
+    $inlaf_tis.mouseenter(function(){
+        let $this = $(this);
+        let i = $this.index();
+        $this.addClass('active').siblings().removeClass('active');
+        $inlaf_l_m.eq(i).addClass('active2').siblings().removeClass('active2');
+    })
+    //关注选项卡
+    let $bg321 = $('#bg321');
+    let $TabCon = $('#TabCon1,#TabCon2');
+    $bg321.on('mouseenter','a',function(){
+        let $this = $(this);
+        let i = $this.index();
+        if(i){
+            $this.parent().attr('class','xixi1');
+            $TabCon.eq(1).show().prev().hide();
+        }else{
+            $this.parent().attr('class','xixi2');
+            $TabCon.eq(0).show().next().hide();
+        }
+    })
 })
